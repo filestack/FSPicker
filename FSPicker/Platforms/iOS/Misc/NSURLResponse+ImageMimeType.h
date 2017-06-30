f89e7f9b35b7cf3e0033ce4e295b7a8194b852d5
Ruben Nine <ruben@9labs.io>
2017-06-30 13:48:36 +0200

    Updating copyright year.

--- FSPicker/Platforms/iOS/Misc/NSURLResponse+ImageMimeType.h
+++ FSPicker/Platforms/iOS/Misc/NSURLResponse+ImageMimeType.h
@@ -3,7 +3,7 @@
 //  FSPicker
 //
 //  Created by Ruben Nine on 30/06/2017.
-//  Copyright © 2016 Filestack. All rights reserved.
+//  Copyright © 2017 Filestack. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
